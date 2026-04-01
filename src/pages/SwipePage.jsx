e1c3ccc2bb707c1e226d88537d4a3fe97e87f86d
Data-six6 <menghong009988@gmai.com>
2026-04-01 23:28:21 +0700

    link swipe and tos

--- src/pages/SwipePage.jsx
+++ src/pages/SwipePage.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import SwipeVenture from "../components/swipeCard";
-
+import { useSearchParams } from "react-router-dom";
 function SwipePage() {
+  const [searchParams] = useSearchParams();
+  const startIndex = parseInt(searchParams.get("start") || "0");
+  console.log("URL param:", searchParams.get("start")); 
+  console.log("startIndex:", startIndex);    
   const [cards] = useState(() => {
     const saved = localStorage.getItem("exploreCards");
     return saved ? JSON.parse(saved) : [];
   });
-  return <main><SwipeVenture cards={cards} /></main>;
+  return <main><SwipeVenture key={startIndex} cards={cards} startindex={startIndex} /></main>;
 }
 
 export default SwipePage;
